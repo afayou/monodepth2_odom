16395018d08589f4013df7bd61ed52ad4aa5a4e2
afayou <1147753060@qq.com>
2020-01-13 16:52:06 +0800

    modify splits/odom del 0~10

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-import torch
+file = open("./splits/odom/test_files_09.txt", "r")
+new_file = open("./splits/odom/test_files_09.txt", "w")
+lines = file.readlines()
+data = []
+del_index = []
+for row in lines:
+    if '0' not in row or '1' not in row or '2' not in row or '3' not in row or '4' not in row or '5' not in row or \
+            '6' not in row or '7' not in row or '8' not in row or '9' not in row or '10' not in row:
+        new_file.write(row)
+new_file.close()
 
-a = torch.rand(2, 1, 3)
-b = torch.rand(2, 1, 3)
-c = torch.cat([a, b], dim=2)  # 按第0维度进行拼接，除拼接之外的维度必须相同
 
-print(a)
-print(b)
-print(torch.split(c, 3, 2))
+
